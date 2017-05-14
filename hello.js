833732349bea832974ecc94356fd0acac5feb571
Viola Jang <angeraa@gmail.com>
2017-05-14 12:35:22 +0900

    first commit

---
+++ hello.js
@@ -0,0 +1,11 @@
+var person = (function person(){
+    var 위 = [];
+
+    return {
+        name : '보라',
+        eat : functon(food){
+        console.log(food + '를 먹는다');
+        위.push(food);
+    };
+}
+}());

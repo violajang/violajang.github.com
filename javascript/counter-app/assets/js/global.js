e710cabcde07d9d16ed64c2ab888a73eb94e3fde
violajang <angeraa@gmail.com>
2018-08-12 15:38:32 +0900

    modify js file

---
+++ javascript/counter-app/assets/js/global.js
@@ -0,0 +1,36 @@
+/*const $ = (dom) =>{
+    return dom.length > 2 
+    ? Array.from(document.querySelectorAll(dom))
+    : document.querySelector(dom);
+};*/
+
+const numValue = document.querySelector('.couter-value');
+const plusCount = document.querySelector('.increment');
+const minusCount = document.querySelector('.decrement');
+const resetCount = document.querySelector('.reset');
+
+let num = 0;
+
+const content = () => {
+  numValue.textContent = num;
+}
+
+const increment = () => {
+  num++;
+  content();
+}
+
+const decrement = () => {
+  if (num === 0) return;
+  num--;
+  content();
+}
+
+const resetNum = () => {
+  num = 0;
+  content();
+}
+
+plusCount.addEventListener('click', increment);
+minusCount.addEventListener('click', decrement);
+resetCount.addEventListener('click', resetNum);
